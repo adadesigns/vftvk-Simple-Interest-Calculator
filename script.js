dbdbc5a66d82c1e128822c6debec7ffb334169eb
adadesigns <102214100+adadesigns@users.noreply.github.com>
2022-03-24 14:10:01 +0200

    final

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
-   // var year = new Date().getFullYear()+parseInt(years);
-    var yearFuture =parseInt(document.getElementById('years').value)+2022;
+    var yearFuture =parseInt(document.getElementById('years').value)+new Date().getFullYear();
     document.getElementById('result').innerHTML='If you deposit <mark>'+principal+',</mark>\<br\>at an interest rate of <mark>'+rate+'</mark>%\<br\>You will receive an amount of <mark>'+interest+'</mark>,\<br\>in the year '+yearFuture;
 }
 //Get rate function
