d12d9b9b8f95d5bcacc6224b09c183b8373ce3ba
adadesigns <102214100+adadesigns@users.noreply.github.com>
2022-03-24 13:54:05 +0200

    final

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-//This function is not used
+//This function is used to present the information what was selected and to calculate the amount
 function compute()
 {
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    var yearFuture =now.getYear() + parseInt(years);
-
+   // var year = new Date().getFullYear()+parseInt(years);
+    var yearFuture =parseInt(document.getElementById('years').value)+2022;
+    document.getElementById('result').innerHTML='If you deposit <mark>'+principal+',</mark>\<br\>at an interest rate of <mark>'+rate+'</mark>%\<br\>You will receive an amount of <mark>'+interest+'</mark>,\<br\>in the year '+yearFuture;
 }
 //Get rate function
 function updateRate() 
